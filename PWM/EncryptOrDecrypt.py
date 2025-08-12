8b0efa103906b2980cf44f6332bf0188ff748446
Ibrahim Faisal <ibrahimfaisal@gmail.com>
2025-08-13 03:09:29 +0500

    Completed Encryption and Decryption logic.
    Added FileIO file.

--- PWM/EncryptOrDecrypt.py
+++ PWM/EncryptOrDecrypt.py
@@ -5,32 +5,32 @@ from cryptography.fernet import Fernet
 
 
 class EncryptOrDecrypyt():
-  def __init__(self, key: str, detail: str, password: str, UserDataObj: dict):
+  def __init__(self, key: str, detail: str, password: str):
     self.key = key
     self.detail = detail
     self.password = password
     self.token = None
-    self.fernetObj = Fernet(self.key)
-
-    #For Decryption
-    self.UserDataObj = UserDataObj
-
-
-  def UserInputconversion(self):
-
-    hashed = hashlib.sha256(self.key.encode()).digest()
-    base64Key = base64.urlsafe_b64encode(hashed)
-    return base64Key
+    self.hashed = hashlib.sha256(self.key.encode()).digest()
+    self.base64Key = base64.urlsafe_b64encode(self.hashed)
+    self.fernetObj = Fernet(self.base64Key)
 
   def encrypt(self):
-    encryptedDetail = self.fernetObj.encrypt(self.detail).decode()
-    encryptedPassword = self.fernetObj.encrypt(self.password).decode()
+    encryptedDetail = self.fernetObj.encrypt(bytes(self.detail, "utf-8")).decode()
+    encryptedPassword = self.fernetObj.encrypt(bytes(self.password, "utf-8")).decode()
+    
 
     return {
       encryptedDetail: encryptedPassword
     }
     
   
-  def decrypt(self):
-    pass
+  def decrypt(self, dictData: dict):
+   
+    encryptedDetail = list(dictData.keys())[0]
+    encryptedPassword = list(dictData.values())[0]
+
+
+    decryptedDetail = self.fernetObj.decrypt(encryptedDetail).decode()
+    decryptedValue = self.fernetObj.decrypt(encryptedPassword).decode()
+    return decryptedDetail, decryptedValue
     
